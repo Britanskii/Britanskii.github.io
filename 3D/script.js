aaac675129ab8224f77b13c3cdb8df74bdc3cb29
Britanskii <73182909+Britanskii@users.noreply.github.com>
2020-12-02 09:39:26 +0300

    Update script.js

--- 3D/script.js
+++ 3D/script.js
@@ -8,8 +8,8 @@
         else if (e.keyCode === 38) rotateX += 4
         else if (e.keyCode === 39) rotateY += 4
         else if (e.keyCode === 40) rotateX -= 4
-        else if (e.keyCode === 219) persp -= 50
-        else if (e.keyCode === 221) persp += 50
+        else if (e.keyCode === 187) persp -= 50
+        else if (e.keyCode === 189) persp += 50
 
         document.querySelector('.cube').style.transform = "rotateY(" + rotateY + "deg) " + "rotateX(" + rotateX + "deg)";
         document.querySelector('.container').style.perspective = persp + "px";
